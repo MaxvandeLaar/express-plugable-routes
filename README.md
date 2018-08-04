dec6a5e2fa565574ab4abb286d4bd17080d3a792
Max van de Laar <max.vandelaar@me.com>
2018-08-04 11:52:24 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# express-plugable-routes
-Load ExpressJS routes dynamic at runtime by placing them into a specified folder
+# Express Plugable Routes
+Load ExpressJS routes dynamic at runtime by placing them into a specified folder.
+
+<ul>
+    <li><a href="#install">Install</a></li>
+    <li><a href="#usage">How to use</a></li>
+    <li><a href="#dependencies">Dependencies</a></li>
+    <li><a href="#license">License</a></li>
+</ul>
+
+<a name="install"></a>
+# Install
+`npm install express-plugable-routes`
+
+`yarn add express-plugable-routes`
+
+<a name="usage"></a>
+# How to use
+
+#### How to initialise the module
+
+In your project, first declare the module
+```javascript 1.8
+const express = require('express');
+const ExpressPlugableRoutes = require('express-plugable-routes');
+
+const app = express();
+
+// add your regular ExpressJS middleware and other logic such as:
+
+/*
+ * Add your 404 error logic here
+ */
+function error404(req, res, next){
+    //...
+}
+app.use(error404);
+
+/*
+ * Add your other error logic here
+ */
+function errorHandler(req, res, next){
+    //...
+}
+app.use(errorHandler);
+```
+
+Next, gather the required parameters and create a new instance
+```javascript
+const addonFolder = `${__dirname}/addons`; // This is the folder where you drop your runtime plugable routes in
+const watchFor = `${addonFolder}/**/config.json`; // The file to watch which contains all the required information about the new route. See Chokidar for more info on this
+const chokidarOpts = {ignored: /^\./, persistent: true}; // The options for Chokidar
+
+new ExpressPlugableRoutes(app, addonFolder, watchFor, chokidarOpts, error404, errorHandler);
+
+// That's it!
+
+module.exports = app;
+```
+
+#### How to list the added routes
+To prevent running and requiring an external database, an 'in-memory' database (LokiJS) is used.
+
+So getting a list of installed routes is easily achieved:
+```javascript
+const Loki = require('lokijs');
+
+const db = new Loki('ExpressPlugableRoutes');
+db.loadDatabase({}, function() {
+    console.log(db.getCollection("plugins").data);
+});
+```
+
+#### Required in the 'config.json'
+The bare minimum should contain:
+```javascript
+{
+    "name": "myCustomRoutes",               // name of the module
+    "main": "index.js",                     // incoming js file to use 
+    "path": "/base-url-of-the-routes",      // base url the module is mounted on
+    "rootFolder": "myCustomRoutesFolder"    // folder path relative to the addon folder up to the main file
+}
+```
+
+<a name="dependencies"></a>
+# Dependencies
+
+* Chokidar https://github.com/paulmillr/chokidar
+* LokiJS https://github.com/techfort/LokiJS
+
+<a name="license"></a>
+# License
+GNU AGPL v3
+
+Copyright (&copy;) 2018 Max van de Laar 
+https://www.maxvandelaar.com
---
+++ index.js
@@ -0,0 +1,100 @@
+const chokidar = require('chokidar');
+const fs = require('fs');
+const Loki = require('lokijs');
+
+class ExpressPlugableRoutes {
+    constructor(app, addonFolder, watchFor, opts = {}, error404 = null, errorHandler = null) {
+        this.db = new Loki('ExpressPlugableRoutes');
+        this.plugins = this.db.addCollection('plugins', {indices: ['name', 'configFile']});
+
+        const watcher = chokidar.watch(watchFor, opts);
+
+        if (error404 !== null) {
+            Object.defineProperty(error404, 'name', {value: 'error404'});
+        }
+        if (errorHandler !== null) {
+            Object.defineProperty(errorHandler, 'name', {value: 'errorHandler'});
+        }
+
+        const self = this;
+
+        watcher
+            .on('add', function (path) {
+                console.log('File', path, 'has been added');
+                self.disableErrors(app, error404, errorHandler);
+                const config = JSON.parse(fs.readFileSync(path, 'utf8'));
+                self.register(app, config, addonFolder, path);
+                self.enableErrors(app, error404, errorHandler);
+                self.saveData();
+            })
+            .on('change', function (path) {
+                console.log('File', path, 'has been changed');
+                self.deregister(app, path);
+
+                self.disableErrors(app, error404, errorHandler);
+                const config = JSON.parse(fs.readFileSync(path, 'utf8'));
+                self.register(app, config, addonFolder, path);
+                self.enableErrors(app, error404, errorHandler);
+                self.saveData();
+            })
+            .on('unlink', function (path) {
+                console.log('File', path, 'has been removed');
+                self.deregister(app, path);
+                self.saveData();
+            })
+            .on('error', function (error) {
+                console.error('Error happened', error);
+            });
+    }
+
+    register(app, config, addonFolder, path) {
+        const route = require(`${addonFolder}/${config.rootFolder}/${config.main}`);
+        Object.defineProperty(route, 'name', {value: config.name});
+        app.use(config.path, route);
+        this.plugins.insert({name: config.name, configFile: path});
+    }
+
+    saveData(){
+        this.db.saveDatabase();
+    }
+
+    deregister(app, path) {
+        const plugins = this.plugins.find({configFile: {$eq: path}});
+        let plugin = null;
+        if (plugins.length > 0){
+           plugin = plugins[0];
+        }
+        if (!plugin){
+            return;
+        }
+        if (plugin) {
+            this.removeByName(app, plugin.name);
+        }
+        this.plugins.remove(plugin);
+    }
+
+    removeByName(app, name) {
+        app._router.stack = app._router.stack.filter(item => item.name !== name);
+    }
+
+    disableErrors(app, error404, errorHandler) {
+        if (error404 !== null) {
+            this.removeByName(app, 'error404');
+        }
+
+        if (errorHandler !== null) {
+            this.removeByName(app, 'errorHandler');
+        }
+    }
+
+    enableErrors(app, error404, errorHandler) {
+        if (error404 !== null) {
+            app.use(error404);
+        }
+        if (errorHandler !== null) {
+            app.use(errorHandler);
+        }
+    }
+}
+
+module.exports = ExpressPlugableRoutes;
